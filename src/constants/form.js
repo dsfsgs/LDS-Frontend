86ae688f3615cc55523af31522720ec7b061d8f2
Taskspeed <taskspeed2002@gmail.com>
2026-09-03 14:01:35 +0800

    constants form

---
+++ src/constants/form.js
@@ -0,0 +1,7 @@
+
+export const formsOptions = [
+  { label: "Learning Application Plan", value: "learning_application_plan", backendKey: "application_plan" },
+  { label: "Learning Implementation Report", value: "learning_implementation_report", backendKey: "implementation_report" },
+  { label: "Learning Application Monitoring Report", value: "learning_application_monitoring_report", backendKey: "application_monitoring_report" },
+  { label: "Leaner Progress Report", value: "leaner_progress_report", backendKey: "progress_report" },
+];
